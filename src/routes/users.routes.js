9401cfa4d76f13354d8c243b57e2de4b7d5e4785
golinskiSt <golinski.st2gmail.com>
2019-08-18 13:57:40 +0200

    Update users.routes.js
    Revamping auth

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -7,19 +7,35 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
+    //Register a user
     const {error} = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    let user = await User.findOne({email: req.body.email});
+    let user = await User.findOne({ email: req.body.password });
     if (user) return res.status(400).send('User already registered.');
 
     user = new User(_.pick(req.body, ['name','email', 'password']));
-    let salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(user.password, salt);
+    user.password = await bcrypt.hash(user.password, 10);
     await user.save();
 
     const token = await user.generateAuthToken()
     res.header('x-auth-token', token).status(201).send({ user, token });
 })
 
+router.post('/login', async(req, res) => {
+    //Login a registered user
+    try {
+        const email = req.body.email;
+        const password = req.body.password
+        const user = await User.findByCredentials(email, password)
+        if (!user) {
+            return res.status(401).send({error: 'Login failed! Check authentication credentials'})
+        }
+        const token = await user.tokens[0].token;
+        res.send(token);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+
+})
 module.exports = router; 
