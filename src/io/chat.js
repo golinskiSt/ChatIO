9d3a18e08b3c9aaf40c76c4a4ead3bfe85598518
golinskiSt <golinski.st2gmail.com>
2019-08-21 20:47:40 +0200

    user array

--- src/io/chat.js
+++ src/io/chat.js
@@ -3,7 +3,21 @@ const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
+class User {
+  constructor (id) {
+    this._id  = id
+    // this._name = name
+}
+set id  (id)  { this._id = id}
+get id  ()       { return this._id}
+// set name (name) { this._name = name}
+// get name ()       { return this._name}
+}
+
+let connections = [];
+
 io.on("connection", function(socket) {
+  connections.push(new User(socket.id));
   socket.on('simple-message', function(message, reciverID) {
     socket.broadcast.to(reciverID).emit('simple-reply', message); 
   });
